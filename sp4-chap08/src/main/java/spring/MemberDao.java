f237d906b695d20021a81911be0da4910e2c0f85
GeneLee <dlgustlr45@naver.com>
2017-08-08 22:56:20 +0900

    MemberDao 구현.

--- sp4-chap08/src/main/java/spring/MemberDao.java
+++ sp4-chap08/src/main/java/spring/MemberDao.java
@@ -15,6 +15,8 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 public class MemberDao {
 	private JdbcTemplate jdbcTemplate;
@@ -45,11 +47,30 @@ public class MemberDao {
 		return results.isEmpty() ? null : results.get(0);
 	}
 	
-	public void insert(Member member){
-		jdbcTemplate.update(
-				"insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE) values (?,?,?,?)",
-				member.getEmail(), member.getPassword(), member.getName(),
-				new Timestamp(member.getRegisterDate().getTime()));
+	public void insert(final Member member){
+		//GeneratedKeyHolder 클래스는 자동 생성된 키 값을 구해주는 KeyHolder 구현 클래스이다.
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		//update 메서드는 PreparedStatementCreator 객체와 KeyHolder 객체를 파라미터로 갖는 메서드이다.
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement(
+						"insert into MEMBER (EMAIL, PASSWORD, NAME, REGDATE)" +
+						"values (?,?,?,?)",
+						new String[]{"ID"}
+						); //두 번째 파라미터로 String 배열인 {"ID"}를 주었는데, 이 두 번째 파라미터는 자동 생성되는 키 칼럼 목록을 지정할 때 사용되며,
+				 		   //우리가 생성한 MEMBER 테이블의 경우 ID칼럼이 자동 증가 키 칼럼이므로  두 번째 파라미터 값으로 {"ID"}를 주었다.
+				pstmt.setString(1,  member.getEmail());
+				pstmt.setString(2,  member.getPassword());
+				pstmt.setString(3,  member.getName());
+				pstmt.setTimestamp(4,  new Timestamp(member.getRegisterDate().getTime()));
+				
+				return pstmt;
+			}
+		}, keyHolder); //단순히 보면 keyHolder는 update() 메서드의 두 번째 파라미터로 전달된 것이다.
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
 	}
 	
 	//기존 update 메서드
