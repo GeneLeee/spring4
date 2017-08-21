0a5e65d89d54c0608b04c5fbb4d535e0e7cc6294
GeneLee <dlgustlr45@naver.com>
2017-08-21 18:23:10 +0900

    테스트 성공.

--- sp4-chap09/src/main/java/chap09/ControllerTest.java
+++ sp4-chap09/src/main/java/chap09/ControllerTest.java
@@ -1,3 +1,4 @@
+
 package chap09;
 
 import org.springframework.stereotype.Controller;
@@ -5,10 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//@Controller 애노테이션이 적용된 클래스를 스프링 MVC에서 컨트롤러로 여러 개 사용할 수 있다.
+
 @Controller //@Controller 애노테이션을 적용한 클래스는 스프링 MVC에서 컨트롤러로 사용된다.
-public class HelloController {
+public class ControllerTest {
 	
-	@RequestMapping("/hello") //@RequsetMapping 애노테이션은 메서드가 처리할 요청 경로를 지정한다. "/hello" 경로로 들어온 요청을 hello() 메서드를 이용해서 처리한다고 설정했다.
+	@RequestMapping("/thisistest")
 	public String hello(Model model, //Model 파라미터 : 컨트롤러의 처리 결과를 뷰에 전달할 떄 사용된다.
 			@RequestParam(value="name", required=false) String name){ //@RequestParam 애노테이션 : HTTP 요청 파라미터 값을 메서드의 파라미터로 전달할 때 사용된다. 이 코드의 경우 name 요청 파라미터의 값을 name 파라미터에 전달한다. 
 		model.addAttribute("greeting", "안녕하세요, " + name); //"greeting"이라는 모델 속성에 값을 설정한다. 값으로는 "안녕하세요, "와 name 파라미터 값을 연결한 문자열을 사용한다. 뒤에서 작성할 JSP 코드는 이 속성을 이용해서 값을 사용한다.
